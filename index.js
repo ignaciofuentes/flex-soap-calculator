ec488083dc6e9b5b8798e218d9f6b0f6ff8d0aaa
Ignacio Fuentes <ignaciofuentes@outlook.com>
2018-12-18 17:44:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2017 Kinvey Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+const sdk = require("kinvey-flex-sdk");
+const handlers = require("./lib/handlers");
+
+// Initiate the Flex SDK Service
+sdk.service((err, flex) => {
+  if (err) {
+    console.log("Error initializing the Flex SDK, exiting.");
+    throw err;
+  }
+
+  const flexFunctions = flex.functions;
+  flexFunctions.register("add", handlers.Add);
+  flexFunctions.register("multiply", handlers.Multiply);
+});
